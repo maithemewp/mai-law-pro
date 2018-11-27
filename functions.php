46e6e61773f1e47169c08d98e1c618a6c10efd78
David Schmeltzle <david@bizbudding.com>
2018-11-27 13:57:20 -0500

    Update functions.php
    added correct syntax

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ add_filter( 'genesis_footer_creds_text', 'maitheme_site_footer_text' );
 function maitheme_site_footer_text( $text ) {
 	$url  = 'https://maitheme.com/';
 	$name = 'Mai Theme';
-	return sprintf( 'Copyright &copy; %s <a href="%s" title="%s">%s</a> &middot; All Rights Reserved &middot; Powered by <a rel="nofollow" rel="noopener" href="%s">%s</a>',
+	return sprintf( 'Copyright &copy; %s <a href="%s" title="%s">%s</a> &middot; All Rights Reserved &middot; Powered by <a rel="nofollow noopener" href="%s">%s</a>',
 		date('Y'),
 		get_bloginfo('url'),
 		get_bloginfo('name'),
